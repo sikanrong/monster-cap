1eea7779ddc5e64a38eefd48825a28863db236e8
Alex Pilafian <sikanrong@Alexs-MacBook-Pro.local>
2015-12-11 20:52:07 +0100

    Modifying the db:pull task to use the local machine's database.yml file to reap details about the local-machine's database

--- lib/monster_cap/tasks/database.rb
+++ lib/monster_cap/tasks/database.rb
@@ -1,34 +1,51 @@
 require 'tempfile'
+
 namespace :db do
   desc 'Pull the remote database to your local database'
   task :pull do
     on primary :db do |host|
+
       SSHKit.config.output_verbosity = Logger::INFO
       database_yml = ''
+
       within current_path do
         database_yml = capture(:cat, 'config/database.yml')
       end
-      database_config = YAML.load(database_yml)
-      remote_database = database_config[fetch(:rails_env).to_s]
-      local_database = database_config['development']
+
+      remote_database_config = YAML.load(database_yml)
+      local_database_config = YAML.load(File.read('config/database.yml'))
+
+      remote_database = remote_database_config[fetch(:rails_env).to_s]
+      local_database = local_database_config['development']
+
       case remote_database['adapter']
-      when 'postgresql'
-        info "Dumping remote database..."
-        remote_file = "/tmp/db.#{Time.now.to_f}-#{rand(99999999999).to_s}.bz2"
-        execute "pg_dump -c -U #{remote_database['username']} #{remote_database['database']} | bzip2 -9 > #{remote_file}"
-        fh = Tempfile.new('db')
-        fh.close
-        download! remote_file, fh.path
-        info "Importing database..."
-        info "bzip2 -cd #{fh.path} | psql -U #{local_database['username']} -d #{local_database['database']}"
-        system "bzip2 -cd #{fh.path} | psql -U #{local_database['username']} -d #{local_database['database']}"
-        fh.unlink
-        execute "rm #{remote_file}"
-      else
-        raise "Adapter #{database_config['adapter']} not supported."
+
+        when 'postgresql'
+
+          info "Dumping remote database..."
+
+          remote_file = "/tmp/db.#{Time.now.to_f}-#{rand(99999999999).to_s}.bz2"
+          execute "pg_dump -c -U #{remote_database['username']} #{remote_database['database']} | bzip2 -9 > #{remote_file}"
+
+          fh = Tempfile.new('db')
+          fh.close
+          download! remote_file, fh.path
+
+          info "Importing database..."
+
+          cmd = "bzip2 -cd #{fh.path} | psql -U #{local_database['username']} -d #{local_database['database']}"
+          info cmd
+          system cmd
+
+          fh.unlink
+          execute "rm #{remote_file}"
+
+        else
+          raise "Adapter #{remote_database_config['adapter']} not supported."
       end
     end
   end
+
   desc 'Push your local database to the remote database'
   task :push do
     on primary :db do |host|
